7197eafb5f64adfc3ef07e445701f378703adc40
RezeScarlet <gui171204@gmail.com>
2024-10-17 10:01:19 -0300

    main menu progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,51 @@
+#include "Color.hpp"
 #include <imgui.h>
+#include <raylib-cpp.hpp>
 #include <raylib.h>
 #include <rlImGui.h>
-#include <raylib-cpp.hpp>
 
+// Got this function from an issue in the Dear ImGui Github
+bool ButtonCenteredOnLine(const char *label, ImVec2 buttonSize,
+                          float alignment = 0.5f) {
+  ImGuiStyle &style = ImGui::GetStyle();
+
+  float size = ImGui::CalcTextSize(label).x + style.FramePadding.x * 2.0f;
+  float avail = ImGui::GetContentRegionAvail().x;
+
+  float off = (avail - buttonSize.x) * alignment;
+  if (off > 0.0f)
+    ImGui::SetCursorPosX(ImGui::GetCursorPosX() + off);
+
+  return ImGui::Button(label, buttonSize);
+}
+
+void TextCenteredOnLine(const char *label, float alignment = 0.5f) {
+  ImGuiStyle &style = ImGui::GetStyle();
+
+  float size = ImGui::CalcTextSize(label).x + style.FramePadding.x * 2.0f;
+  float avail = ImGui::GetContentRegionAvail().x;
+
+  float off = (avail - size) * alignment;
+  if (off > 0.0f)
+    ImGui::SetCursorPosX(ImGui::GetCursorPosX() + off);
+
+  ImGui::Text("%s", label);
+}
+
+void ColorButton(float h, float s, float v);
 int main() {
 
   SetConfigFlags(FLAG_MSAA_4X_HINT);
   raylib::Window window(854, 480);
   SetTargetFPS(60);
 
+  float h = window.GetHeight();
+  float w = window.GetWidth();
+  ImVec2 wh(w, h);
+
   rlImGuiSetup(true);
+
+  ImVec2 buttonSize(w / 5, h / 8);
 
   while (WindowShouldClose() == false) {
 
@@ -17,8 +53,40 @@ int main() {
     rlImGuiBegin();
 
     ClearBackground(raylib::Color::White());
-  
-    ImGui::ShowDemoWindow();
+
+    // Chose style
+    // ImGui::SetNextWindowBgAlpha(0);
+    ImGui::SetNextWindowPos(ImVec2(0, 0));
+    ImGui::SetNextWindowSize(wh);
+    if (ImGui::Begin("Main Menu", __null,
+                     ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize |
+                         ImGuiWindowFlags_NoTitleBar)) {
+      ImGui::Dummy(buttonSize);
+      ImGui::SetWindowFontScale(4);
+      TextCenteredOnLine("Estruturas Lineares");
+      ImGui::SetWindowFontScale(2);
+      ImGui::Dummy(buttonSize);
+      ImGui::PushStyleColor(ImGuiCol_Button,
+                            (ImVec4)ImColor::HSV(343, 0.5269, 0.9232));
+      ImGui::PushStyleColor(ImGuiCol_ButtonHovered,
+                            (ImVec4)ImColor::HSV(343, 0.5269, 0.9932));
+      ImGui::PushStyleColor(ImGuiCol_ButtonActive,
+                            (ImVec4)ImColor::HSV(343, 0.5269, 0.8232));
+      if (ButtonCenteredOnLine("Pilha", buttonSize)) {
+        // Link to file
+      }
+      ImGui::PopStyleColor(3);
+      if (ButtonCenteredOnLine("Fila", buttonSize)) {
+        // Link to file
+      }
+      if (ButtonCenteredOnLine("Deque", buttonSize)) {
+        // Link to file
+      }
+      if (ButtonCenteredOnLine("Lista", buttonSize)) {
+        // Link to file
+      }
+    }
+    ImGui::End();
 
     rlImGuiEnd();
     EndDrawing();
