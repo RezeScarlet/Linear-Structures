1c5f8e8d8af193c28938bf248058c9c249934b0f
RezeScarlet <gui171204@gmail.com>
2024-10-18 19:26:59 -0300

    Functions to make style easier

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <imgui.h>
 
+void StyleButton(int mode);
+void PopStyleColor(int style, int color);
 bool ButtonCenteredOnLine(const char *label, ImVec2 buttonSize,
                           float alignment = 0.5f);
 
--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,6 +1,29 @@
 #include <imgui.h>
 
 // Got this function from an issue in the Dear ImGui Github
+void StyleButton(int mode) {
+  ImVec4 colors[] = {
+      ImColor(235, 111, 146), // Red
+      ImColor(235, 188, 186), // Pink
+      ImColor(196, 167, 231), // Purple
+      ImColor(156, 207, 216), // Turquoise
+  };
+
+
+  ImGui::PushStyleVar(ImGuiStyleVar_FrameBorderSize, 1);
+
+  ImGui::PushStyleColor(ImGuiCol_Text, colors[mode - 1]);
+  ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor(64, 61, 82));
+  ImGui::PushStyleColor(ImGuiCol_ButtonHovered, (ImVec4)ImColor(82, 79, 103));
+  ImGui::PushStyleColor(ImGuiCol_ButtonActive, (ImVec4)ImColor(33, 32, 46));
+  ImGui::PushStyleColor(ImGuiCol_Border, colors[mode - 1]);
+}
+
+void PopStyleColor(int style, int color) {
+  ImGui::PopStyleVar(style);
+  ImGui::PopStyleColor(color);
+}
+
 bool ButtonCenteredOnLine(const char *label, ImVec2 buttonSize,
                           float alignment = 0.5f) {
   ImGuiStyle &style = ImGui::GetStyle();
